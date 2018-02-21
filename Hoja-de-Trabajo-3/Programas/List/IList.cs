121d9a9a8fff4c552a1b16af386a3a73ffb9b08f
Dylan Rodas <rodas171315@unis.edu.gt>
2018-02-20 23:31:48 -0600

    Se empieza Hoja 3

--- Hoja-de-Trabajo-3/Programas/List/IList.cs
+++ Hoja-de-Trabajo-3/Programas/List/IList.cs
@@ -4,21 +4,43 @@ namespace IList
 {
     public interface IList<T>
     {
-        bool Get(int indice, out int )
+        public bool Get(int indice, out int resultado)
         {
             
         }
 
-        bool Set(int indice, T elemento)
+        public bool Set(int indice, T elemento)
         {
 
         }
         
-        void Push(T elemento)
+        public void Push(T elemento)
         {
+            IList<T> lista = this;
 
+            while(lista.Resto != null)
+            {
+                lista = lista.Resto;
+            }
+
+            lista.Resto = new IList<T>(elemento);            
         }
 
-        int Length{get;}
+        public int Length
+        { get
+            {
+                Ilist<T> lista = this;
+                int length = 0;
+
+                while(lista.Resto != null)
+                {
+                    lista = lista.Resto;
+                    length++;
+                }
+            }
+                return length;                
+            }
+        }
+
     }
 }
