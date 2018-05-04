17472b1c8d9291466e0d22345591b58fdd0094d5
Dylan Rodas <rodas171315@unis.edu.gt>
2018-05-04 07:08:12 -0600

    Agregando Hojas de Trabajo 9 y 10, incluyendo los archivos necesarios para elaborarlas.

---
+++ Hoja-de-Trabajo-9/Programas/Heap/Heap.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Heap
+{
+    class Heap
+    {
+      public void ShiftDown(int[] arreglo, int indice)
+      {
+          indice = 0;
+
+          
+      }  
+
+    }
+}
