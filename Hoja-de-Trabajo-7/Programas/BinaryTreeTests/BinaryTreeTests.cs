70233a8e49666979780632fa13b58b76a72a9cf0
Dylan Rodas <rodas171315@unis.edu.gt>
2018-05-07 12:00:15 -0600

    Agregando mas tests a la Hoja de Trabajo 7.

--- Hoja-de-Trabajo-7/Programas/BinaryTreeTests/BinaryTreeTests.cs
+++ Hoja-de-Trabajo-7/Programas/BinaryTreeTests/BinaryTreeTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Xunit;
 
 namespace BinaryTreeTests
@@ -34,7 +35,7 @@ namespace BinaryTreeTests
         }
 
         [Fact]
-        public void TestInsert()
+        public void TestInsert1()
         {
             IBinTree sut = new BinaryTree(100);
             sut.Insert(95);
@@ -52,5 +53,39 @@ namespace BinaryTreeTests
 
             Assert.Equal(new int[] { 5,15,25,35,45,55,65,75,85,95,100 }, result);
         }
+
+        [Fact]
+        public void TestInsert2()
+        {
+            IBinTree sut = new BinaryTree(10);
+
+            int[] TestValores = {42, 50, 18};
+
+            for(int i = 0; i < TestValores.Length; i++)
+            {
+                sut.Insert(TestValores[i]);
+            }
+
+            int[] resultado = sut.ToArray();
+
+            for(int i = 0; i < resultado.Length; i++)
+            {
+                Assert.True(resultado[i] <= resultado[i+1]);
+            }
+        }
+
+        [Fact]
+        public void TestToArray()
+        {
+            IBinTree<int> sut = new BinaryIntTree(
+                5
+                , new BinaryTree<int>(1)
+                , new BinaryIntTree(33));
+
+            int[] actual = sut.ToArray();
+            int[] expected = new int[]{33,5,1};
+
+            Assert.Equal(expected, actual);
+        }                                
     }
 }
